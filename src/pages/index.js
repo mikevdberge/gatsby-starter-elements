f2cabfbf817d2ff2551f7126b02d0cc9301afdae
Mike van den Berge <mikevdberge@users.noreply.github.com>
2021-09-23 08:59:03 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ require('@stoplight/elements/styles.min.css');
 export default function Home() {
   return <div>
       <API 
-        apiDescriptionUrl="https://raw.githubusercontent.com/mikevdberge/safelinks/master/reference/schema.json?token=ADC2BRGIDWRT7LP3LXCXFJLBJQP7A"
+        apiDescriptionUrl="https://raw.githubusercontent.com/mikevdberge/safelinks/master/reference/schema.json"
         router={typeof window === 'undefined' ? 'memory' : 'history'} 
       />
     </div>
